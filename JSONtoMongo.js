09c44ef7b2e50612dbaf0fd6ade5e83d2080433c
joshuadargan <joshuadargan282@ufl.edu>
2019-09-14 22:23:10 -0400

    Bootcamp 2 complete

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,18 +9,38 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+var listingData;
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
-  //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+//Reads in listings.json to save the entries to the database
+fs.readFile('listings.json', 'utf8', function(err, data) {
 
-  Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
- */
+  //Check for errors
+  if (err) throw err;
 
+  //Save the sate in the listingData variable already defined
+  listingData = JSON.parse(data);
+
+  //For each listing entry, save it to the database
+  listingData.entries.forEach(function(element) {
+    var listingEntry = new Listing({
+      name: element.name,
+      code: element.code,
+      coordinates: element.coordinates,
+      address: element.address
+    });
+    //Saves the listing entry to the database
+    listingEntry.save(function(err) {
+      if (err) throw err;
+
+      console.log("Saved listing successfully");
+    });
+  });
+});
 
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,6 +1,14 @@
 /* Add all the required libraries*/
+var mongoose = require('mongoose'), 
+Schema = mongoose.Schema, 
+Listing = require('./ListingSchema.js'), 
+config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,6 +18,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({name: "Library West"}, function(err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+
 };
 var removeCable = function() {
   /*
@@ -17,20 +30,37 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({ code: 'CABL' }, function(err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
 };
-var updatePhelpsLab = function() {
+var updatePhelpsMemorial = function() {
   /*
     Phelps Lab address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
     
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
-   */
+  */
+  Listing.updateOne({ name: 'Phelps Laboratory' }, { address: '1953 Museum Rd, Gainesville, FL 32603' });
+  Listing.find({name: "Phelps Laboratory"}, function(err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+   Listing.find({}, function(err, users) {
+    if (err) throw err;
+  
+    // object of all the users
+    console.log(users);
+  });
+
 };
 
 findLibraryWest();
